bf54fc33a9eff8f1b2e5f97a8caa5ad078a27451
amilcosta <acosta.experience@gmail.com>
2026-02-27 00:31:47 -0300

    Update terminos and sign up

--- app/planes/page.tsx
+++ app/planes/page.tsx
@@ -10,11 +10,22 @@ export default async function PlanPage() {
         <div className="min-h-screen bg-gray-50">
             <Navigation />
             <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                <div className="w-[100%] flex text-secondary-foreground justify-center mb-[2em]">
+                    <div className="  w-[70%]">
+                        <span className="w-[100%] text-2xl font-bold block text-center"> "El Cofre de la Marmota" </span>
+                        <span className="w-[100%] block text-secondary-foreground high-font">
+                            <p><strong>El Cofre de la marmota</strong> es una sección que podrá ser visualizada en un grupo privado de Telegram, donde se informará de todo lo que 
+                            ofrecerá Marmota Pay de gestion propia. </p>
+                            <p>Para poder enterarse de estas novedades, se requiere ser parte del plan Premium, el cual se comenta en la sección Planes, que aparece a continuación</p>
+
+                        </span>
+                    </div>
+                </div>
                 <div className="w-[100%] flex text-secondary-foreground justify-center">
-                    <div className="  w-[60%]">
+                    <div className="  w-[70%]">
                         <span className="w-[100%] text-2xl font-bold block text-center"> Planes </span>
-                        <span className="w-[100%] block text-secondary-foreground">
-                            <p><strong>Marmota pay</strong> te ofrece la oportunidad de escoger un plan de notificaciones de las ofertas de videojuegos asociadas 
+                        <span className="w-[100%] block text-secondary-foreground high-font">
+                            <p><strong>Marmota Pay</strong> te ofrece la oportunidad de escoger un plan de notificaciones de las ofertas de videojuegos asociadas 
                             a las tiendas que tenemos registradas, con el fin de poder ayudarte a conocer cuando una tienda ha bajado de precio de algun videojuego.</p>
                             <p> </p>
 
@@ -25,7 +36,7 @@ export default async function PlanPage() {
                     Escoge el plan que se adapte a ti
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
-                    <div className="w-[100%] mt-12 text-secondary-foreground">
+                    <div className="w-[100%] mt-12 text-secondary-foreground ">
                         <Card className="bg-squirtle-gray rounded-md text-secondary-foreground">
                             <CardHeader>
                                 <CardTitle className="text-center text-secondary-foreground">Plan Gratis</CardTitle>
@@ -55,16 +66,17 @@ export default async function PlanPage() {
                                     </div>
                                     <div className="float-left display-block w-[93%]">
                                         <span className="text-gray-600 text-base"> Recibir notificacion sobre 3 videojuegos aleatorios que han bajado de precio 
-                                        en canal gratis de Telegram</span>
+                                        en los canales gratis de Telegram que te hayas asociado.</span>
                                     </div>
                                 </div>
                                 <div className="mt-9">
-                                    <Button  className="cursor-pointer w-[50%] small-font bg-accent block m-auto">
-                                        <a href="/auth/signup" target="_blank" rel="noopener noreferrer">
-                                            Registrarse
-                                        </a>
-                                    </Button>   
+                                    <a href="https://t.me/+vnfYFsFYYHQ2OGUx" target="_blank" rel="noopener noreferrer">
+                                        <Button  className="cursor-pointer w-[50%] small-font bg-accent block m-auto">
+                                            Unirse a canal
+                                        </Button>
+                                    </a>   
                                 </div>
+                                
                             </CardContent>
                         </Card>
                     </div>
@@ -97,8 +109,32 @@ export default async function PlanPage() {
                                         </svg>
                                     </div>
                                     <div className="float-left display-block w-[93%]">
-                                    <span className="text-gray-600 pl-1 text-base"> Recibir notificacion de  <strong>todos</strong> los videojuegos que han bajado de precio 
-                                        en los canales premium de Telegram clasificado por consola de videojuegos
+                                    <span className="text-gray-600 pl-1 text-base"> Recibir notificación de  <strong>todos</strong> los videojuegos que han bajado de precio 
+                                        en el canal premium de Telegram clasificado por consola de videojuegos, asi como tambien otras categorías.
+                                    </span>
+                                    </div>
+                                </div>
+                                <div className="flex mt-2">
+                                    <div className="float-left display-block w-plan-check">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00b090">
+                                        <path d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z"/>
+                                        <path d="M16.293 8.293 11 13.586l-2.293-2.293-1.414 1.414L11 16.414l6.707-6.707-1.414-1.414z"/>
+                                        </svg>
+                                    </div>
+                                    <div className="float-left display-block w-[93%]">
+                                    <span className="text-gray-600 pl-1 text-base"> Sección "El Cofre de la Marmota"
+                                    </span>
+                                    </div>
+                                </div>
+                                <div className="flex mt-2">
+                                    <div className="float-left display-block w-plan-check">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00b090">
+                                        <path d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z"/>
+                                        <path d="M16.293 8.293 11 13.586l-2.293-2.293-1.414 1.414L11 16.414l6.707-6.707-1.414-1.414z"/>
+                                        </svg>
+                                    </div>
+                                    <div className="float-left display-block w-[93%]">
+                                    <span className="text-gray-600 pl-1 text-base"> Ofertas de Steam en el grupo de Telegram Premium
                                     </span>
                                     </div>
                                 </div>
@@ -115,11 +151,11 @@ export default async function PlanPage() {
                                     </div>
                                 </div>
                                 <div className="mt-9">
-                                    <Button  className="cursor-pointer w-[50%] small-font bg-accent block m-auto">
-                                        <a href="/auth/signup" target="_blank" rel="noopener noreferrer">
-                                            Registrarse
-                                        </a>
-                                    </Button>   
+                                    <a href="/auth/signup" target="_blank" rel="noopener noreferrer">
+                                        <Button  className="cursor-pointer w-[50%] small-font bg-accent block m-auto">
+                                                Registrarse
+                                        </Button>  
+                                    </a> 
                                 </div>
                             </CardContent>
                         </Card>
