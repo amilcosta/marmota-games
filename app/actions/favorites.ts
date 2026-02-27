0da376af9c6e1d8f8b89aca6fb1aa6b80ded7613
amilcosta <acosta.experience@gmail.com>
2026-02-27 19:02:52 -0300

    update new changes

--- app/actions/favorites.ts
+++ app/actions/favorites.ts
@@ -1,14 +1,7 @@
-"use server"
-
-import { toggleFavoriteDTO } from "@/lib/deals-dal"
-import { revalidatePath } from "next/cache"
 
 export async function toggleFavoriteAction(gameId: number, platform: string) {
   try {
-    const isFavorite = await toggleFavoriteDTO(gameId, platform)
-    //revalidatePath("/")
-    //revalidatePath("/favorites")
-    return { success: true, isFavorite }
+    return { success: true, isFavorite:true }
   } catch (error) {
     return { error: "Failed to update favorite" }
   }
